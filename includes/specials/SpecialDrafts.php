c352265088288e1d06206696eb44f1553e3a4d23
Antoine Mercier-Linteau <antoine@bitsofmymind.com>
2020-03-13 01:05:22 -0400

    Add page delete capacity and fix other bugs

--- includes/specials/SpecialDrafts.php
+++ includes/specials/SpecialDrafts.php
@@ -45,7 +45,11 @@ class SpecialDrafts extends \FormSpecialPage
         
         if($val = $request->getVal('success', false))
         {
-            $t.= '<div class="success">Le brouillon [['.$user->getUserPage()->getText().'/'.$val.'|'.$val.']] a été créé avec succès</div>';
+            $t.= '<div class="success">Le brouillon [['.$user->getUserPage()->getFullText().'/Brouillons/'.$val.'|'.$val.']] a été créé avec succès.</div>'."\n";
+        }
+        else if($val = $request->getVal('delete-success', false))
+        {
+            $t.= '<div class="success">Le brouillon [['.$user->getUserPage()->getFullText().'/Brouillons/'.$val.'|'.$val.']] a été supprimé.</div>'."\n";
         }
         
         // Retrieve all the subpages from the database.
@@ -86,11 +90,23 @@ class SpecialDrafts extends \FormSpecialPage
                 $t .= '<td>'.\DateTime::createFromFormat('U', wfTimestamp(TS_UNIX, \Revision::getTimestampFromId($draft, $draft->getLatestRevID())), (new \DateTime())->getTimezone())->format('Y/m/d H:i:s').' </td>';
                 
                 $t .= '<td><html>';
-                $form = new OOUI\FormLayout();
+                $form = new OOUI\FormLayout([
+                    'method' => 'post', 
+                    'action' => $this->getFullTitle()->getFullURL(),
+                    'enctype' => "application/x-www-form-urlencoded"
+                ]);
                 
                 $form->addItems([
                     new OOUI\HorizontalLayout(['label' => '', 'classes' => 'container', 'items' => [
-                        new OOUI\FieldLayout(new OOUI\ButtonInputWidget(['type' => 'submit', 'label' =>'Supprimer', 'flags' => ['destructive']])),
+                        new OOUI\HiddenInputWidget(['value' => $draft->getSubpageText(), 'name' => 'wpname']),
+                        new OOUI\HiddenInputWidget(['value' => $user->getEditToken(), 'name' => 'wpEditToken']),
+                        new OOUI\FieldLayout((new OOUI\ButtonInputWidget([
+                            'type' => 'submit', 
+                            'label' =>'Supprimer',
+                            'name' => 'wpDelete',
+                            'value' => 1,
+                            'flags' => ['destructive']
+                        ]))->addClasses(['draft-delete'])),
                         new OOUI\FieldLayout(new OOUI\ButtonWidget([
                             'label' =>'Renommer', 
                             'target' => '_blank', 
@@ -104,7 +120,17 @@ class SpecialDrafts extends \FormSpecialPage
         }
         
         $t .= "</table>\n";
+        
         $t .= "== Nouveau brouillon ==\n__NOEDITSECTION__";
+        
+        // Add a JS confirm dialog to the delete buttons.
+        $t .= '<html><script type="text/javascript">
+window.addEventListener("load", function(){
+    $(".draft-delete").on("click", function(){
+        return confirm("Êtes-vous certain de vouloir supprimer cette page?");
+    });
+});
+</script></html>';
         
         $parser = MediaWikiServices::getInstance()->getParser();
         
@@ -168,98 +194,43 @@ class SpecialDrafts extends \FormSpecialPage
      * */
     public function onSubmit($data)
     {
-        $class = \Title::newFromText($data['class'].'/Prototype', NS_CLASS);
-        
-        // Do some validation.
-        if(!$class->exists()) { return 'Classe invalide ou ne contient pas de prototype'; }
-        if(MediaWikiServices::getInstance()->getPermissionManager()->userCan('view', $this->getUser(), $class))
-        {
-            return 'Vous n\'êtes pas autorisés à utiliser cette classe';
-        }
-        
-        $prototype = \Article::newFromTitle($class, \RequestContext::getMain());
+        if(!$data['name']) { return 'Nom de page manquant'; } // If a name was not provided.
         $draft = \Article::newFromTitle(\Title::newFromText($this->getUser()->getName().'/Brouillons/'.ucfirst($data['name']), NS_USER), \RequestContext::getMain())->getPage();
         
+        if($this->getRequest()->getVal('wpDelete', false)) // If this is a request to delete a page.
+        {
+            $errors = [];
+            if(!$draft->exists()) { return 'La page n\'existe pas'; }
+            if(!$draft->doDeleteArticle('Suppression du brouillon', false, null, null, $errors, $this->getUser(), true))
+            {
+                return 'La suppression de la page a échouée.';
+            }
+            
+            $this->getOutput()->redirect($this->getFullTitle()->getFullURL(['delete-success' => $draft->getTitle()->getSubpageText()]));
+            return true;
+        }
+        
+        // Else this is request to create a draft.
+        if($data['class'] != 'empty')
+        {
+            $class = \Title::newFromText($data['class'].'/Prototype', NS_CLASS);
+            
+            // Do some validation.
+            if(!$class->exists()) { return 'Classe invalide ou ne contient pas de prototype'; }
+            if(MediaWikiServices::getInstance()->getPermissionManager()->userCan('view', $this->getUser(), $class))
+            {
+                return 'Vous n\'êtes pas autorisés à utiliser cette classe';
+            }
+            
+            $prototype = \Article::newFromTitle($class, \RequestContext::getMain());
+            $content = $prototype->getRevision()->getContent()->getNativeData();
+            $content = str_replace(['<includeonly>' , '</includeonly>'], '', $content);
+        }
+        else { $content = ''; } // The user wanted a blan page.
+        
         // Save the new draft.
-        $content = \ContentHandler::makeContent( $prototype->getRevision()->getContent()->getNativeData(), $draft->getTitle());
-        $content = str_replace(['<noinclude>' , '</noinclude>'], '', $content);
-        $status = $draft->doEditContent($content, 'Création du brouillon', 0, false, $this->getUser());
+        $status = $draft->doEditContent(\ContentHandler::makeContent( $content, $draft->getTitle()), 'Création du brouillon', 0, false, $this->getUser());
         
-        $this->getOutput()->redirect($this->getURL(['success' => $draft->getTitle()->getText()]));
+        $this->getOutput()->redirect($this->getFullTitle()->getFullURL(['success' => $draft->getTitle()->getSubpageText()]));
     }
-    
-    /**
-     * @inheritdoc
-     * */
-    /*public function execute( $params ) 
-    {
-        $request = $this->getRequest();
-        $output = $this->getOutput();
-        $output->enableOOUI();
-        $user = $this->getUser();
-        $db = wfGetDB( DB_REPLICA ); //\MediaWiki\MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection(DB_REPLICA);
-        $this->setHeaders();
-        
-        // Retrieve all the subpages from the database.
-        $titleArray = \TitleArray::newFromResult($db->select( 
-            'page',
-            [ 'page_id', 'page_namespace', 'page_title', 'page_is_redirect' ],
-            [
-                'page_is_redirect' => 1, 
-                'page_namespace' => NS_USER,
-                'page_title'.$db->buildLike($user->getName().'/', $db->anyString())
-            ],
-            __METHOD__,
-            []
-            )
-        );
-        
-        // Build the HTML / Wikicode of the page.
-        
-        $t = "== Brouillons en cours ==
-{| class=\"wikitable\"
-|-
-! Titre !! Dernière modification !! Actions
-|-      \n";
-        
-        if(!$titleArray->count()) // There are no drafts.
-        {
-            $t .= "|colspan=\"3\"|''Aucun brouillon''\n|}";
-        }
-        else
-        {
-        }
-        
-        $t .= "\n== Nouveau brouillon ==";
-        $output->addWikiTextAsContent($t);
-        
-        $form = new OOUI\FormLayout();
-        
-        $classes = new OOUI\DropdownInputWidget();
-        $classes->setTitle('Sélectionnez une classe')->setOptions([[
-                'data' => 'a',
-                'label' => 'Maladie'
-            ]
-        ]);
-        
-        $form->addItems([
-            new OOUI\FieldsetLayout(['label' => '', 'classes' => 'container', 'items' => [
-                new OOUI\FieldLayout($classes, [
-                    'label' => 'Classe de la page', 
-                    'align' => 'top']),
-                new OOUI\FieldLayout(new OOUI\TextInputWidget([
-                    'name' => 'titre', 
-                    'placeholder' => 'ex: Pneumonie', 
-                    'maxLength' => 30, 
-                    'required' => true,
-                ]), [
-                    'label' => 'Nom de la page', 
-                    'align' => 'top',
-                    'help' => 'Ce nom sera utilisé comme titre de la page. La page sera ensuite disponible dans la liste de vos brouillons sous ce nom.'
-                ]),
-                new OOUI\FieldLayout(new OOUI\ButtonInputWidget(['type' => 'submit', 'label' =>'Créer']))
-            ]])   
-        ]);
-        $output->addHTML($form);
-    }*/
 }
